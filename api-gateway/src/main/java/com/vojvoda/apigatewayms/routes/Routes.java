507b8f5e65e85fc98417860b6b84d877ec40d081
Leotrim Vojvoda <leotrim.sd@gmail.com>
2024-09-26 10:26:21 +0200

    Fixed Database

--- api-gateway/src/main/java/com/vojvoda/apigatewayms/routes/Routes.java
+++ api-gateway/src/main/java/com/vojvoda/apigatewayms/routes/Routes.java
@@ -1,5 +1,6 @@
 package com.vojvoda.apigatewayms.routes;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.gateway.server.mvc.handler.GatewayRouterFunctions;
 import org.springframework.cloud.gateway.server.mvc.handler.HandlerFunctions;
 import org.springframework.context.annotation.Bean;
@@ -9,12 +10,51 @@ import org.springframework.web.servlet.function.*;
 @Configuration
 public class Routes {
 
+
+    @Value("${product-service-url}")
+    private String PRODUCT_SERVICE_URL;
+
+    @Value("${order-service-url}")
+    private String ORDER_SERVICE_URL;
+
+    @Value("${inventory-service-url}")
+    private String INVENTORY_SERVICE_URL;
+
+    @Value("${product-service-base-path}")
+    private String PRODUCT_SERVICE_BASE_PATH;
+
+    @Value("${order-service-base-path}")
+    private String ORDER_SERVICE_BASE_PATH;
+
+    @Value("${inventory-service-base-path}")
+    private String INVENTORY_SERVICE_BASE_PATH;
+
+
+
     @Bean
     public RouterFunction<ServerResponse> productServiceRoute() {
 
         return GatewayRouterFunctions.route("product_service")
-                .route(RequestPredicates.path("/api/product"),
-                        HandlerFunctions.http("http://localhost:8080"))
+                .route(RequestPredicates.path(PRODUCT_SERVICE_BASE_PATH),
+                        HandlerFunctions.http(PRODUCT_SERVICE_URL))
+                .build();
+    }
+
+    @Bean
+    public RouterFunction<ServerResponse> orderServiceRoute() {
+
+        return GatewayRouterFunctions.route("order_service")
+                .route(RequestPredicates.path(ORDER_SERVICE_BASE_PATH),
+                        HandlerFunctions.http(ORDER_SERVICE_URL))
+                .build();
+    }
+
+    @Bean
+    public RouterFunction<ServerResponse> inventoryServiceRoute() {
+
+        return GatewayRouterFunctions.route("inventory_service")
+                .route(RequestPredicates.path(INVENTORY_SERVICE_BASE_PATH),
+                        HandlerFunctions.http(INVENTORY_SERVICE_URL))
                 .build();
     }
 }
