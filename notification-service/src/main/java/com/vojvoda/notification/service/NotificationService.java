65f5d6ad209adcf37a39fc753ff45dc83b185405
Leotrim Vojvoda <leotrimvojvoda@gmail.com>
2024-11-09 20:46:04 +0100

    Observation

--- notification-service/src/main/java/com/vojvoda/notification/service/NotificationService.java
+++ notification-service/src/main/java/com/vojvoda/notification/service/NotificationService.java
@@ -1,5 +1,6 @@
 package com.vojvoda.notification.service;
 
+import com.vojvoda.order.event.OrderPlacedEvent;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.kafka.annotation.KafkaListener;
@@ -24,14 +25,17 @@ public class NotificationService {
         MimeMessagePreparator message = mimeMessage -> {
             MimeMessageHelper messageHelper = new MimeMessageHelper(mimeMessage);
             messageHelper.setFrom("springshop@email.com");
-            messageHelper.setTo(orderPlacedEvent.getEmail());
+            messageHelper.setTo(orderPlacedEvent.getEmail().toString());
             messageHelper.setSubject(String.format("Your Order with OrderNumber %s is placed successfully",
                     orderPlacedEvent.getOrderNumber()));
             messageHelper.setText(String.format("""
+                            Hello %s, %s
                             Your order with order number %s is now placed successfully.
                             Best Regards
                             Spring Shop
                             """,
+                    orderPlacedEvent.getFirstName(),
+                    orderPlacedEvent.getLastName(),
                     orderPlacedEvent.getOrderNumber()));
         };
         try {
