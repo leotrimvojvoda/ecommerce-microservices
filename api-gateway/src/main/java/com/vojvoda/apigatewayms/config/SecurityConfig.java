13dba29f9f30a0a42b093936f6ba909c8753359a
Leotrim Vojvoda <leotrimvojvoda@gmail.com>
2024-11-09 17:48:28 +0100

    Observation

--- api-gateway/src/main/java/com/vojvoda/apigatewayms/config/SecurityConfig.java
+++ api-gateway/src/main/java/com/vojvoda/apigatewayms/config/SecurityConfig.java
@@ -21,7 +21,8 @@ public class SecurityConfig {
             "/v3/api-docs/**",
             "/swagger-resources/**",
             "/api-docs/**",
-            "/aggregate/**"
+            "/aggregate/**",
+            "/actuator/prometheus"
     };
 
     @Bean
